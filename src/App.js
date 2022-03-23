0ed2830a1a752a84a7b326b1438f1bff2a8aa1ba
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-03-24 00:40:56 +0900

    update: Separate Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Hello React Again</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Hello Again Docker !!
+      </a>
+    </header>
+  )
+}
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React Again</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Again Docker !!
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
