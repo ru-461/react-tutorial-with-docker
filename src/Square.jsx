337347c1615e57b462c112edda0061c193559e64
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-04-04 21:48:53 +0900

    update: Rename Square component

---
+++ src/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+const Square = () => {
+  return(
+    <button className='square'>
+    </button>
+  )
+}
+
+export default Square;
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import ReactDOM  from 'react';
-import './index.css';
